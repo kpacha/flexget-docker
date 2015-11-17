323243c17f9d61a90601a2547a22b9739a2238fe
Kpacha <kpacha666@gmail.com>
2015-11-17 21:02:47 +0100

    subtitle lang placeholder replaced on start

--- flexget/start.sh
+++ flexget/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sed "s@##RSS_URL##@$RSS_URL@" </root/flexget/config_base.yml >/root/.flexget/config.yml
+sed "s@__RSS_URL__@$RSS_URL@" </root/flexget/config_base.yml >/root/.flexget/config.yml
+sed -i "s@__SUBTITLES_LANGUAGE__@$SUBTITLES_LANGUAGE@" /root/.flexget/config.yml
 
 /usr/local/bin/flexget "$@"
